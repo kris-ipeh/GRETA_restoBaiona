effdda4d485e4a025c981a64fdad9d61b191dca7
Sébastien Ducoulombier <seb@ldd.fr>
2017-04-21 10:10:51 +0200

    ajout du titre

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html>
  <head>
-  <title>Restaurants des quartiers de la ville</title>
+  <title>Site des restaurants des quartiers de la ville</title>
   <meta charset="utf-8">
  </head>
  <body>
+  <h1>Site des restaurants des quartiers de la ville</h1>
   Hop™ !
  </body>
 </html>
