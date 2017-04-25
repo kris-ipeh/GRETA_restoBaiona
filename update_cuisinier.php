421e75e4fbef3ddc90c8da9bbbce61365c297db6
Kris <chrisparis.eh@gmail.com>
2017-04-25 14:12:50 +0200

    formulaire modifier cuisinier

---
+++ update_cuisinier.php
@@ -0,0 +1,73 @@
+<?php include('entete.php'); ?>
+
+<?php
+$sql = 'SELECT restaurant.nom AS restaurant FROM restaurant';
+$request = $base->prepare($sql);
+$request->execute();
+?>
+
+<form action="modifier_cuisinier.php" method="post">
+  <fieldset id="modifier_cuisinier">
+  <legend>Modifier les informations d'un cuisinier</legend>
+
+    <div>
+      <label for="nom_cuisinier">Nom du cuisinier :</label>
+      <input type="text" name="nom_cuisinier" id="nom_cuisinier">
+    </div>
+
+    <div>
+      <label for="salaire">salaire :</label>
+      <input type="number" name="salaire" id="salaire" value="" />
+    </div>
+
+    <div>
+      <label for="restaurant_cuisinier">Restaurant du cuisinier :</label>
+      <select id="restaurant_cuisinier">
+        <?php
+          while($ligne = $request->fetch()){ ?>
+            <option>
+            <?= $ligne['restaurant']; ?>
+            </option>
+        <?php } ?>
+      </select>
+    </div>
+<?php
+$sql = 'SELECT DISTINCT diplome.nom AS diplome FROM diplome';
+$request = $base->prepare($sql);
+$request->execute();
+?>
+    <div>
+      <label for="diplome_cuisinier">Diplôme du cuisinier :</label>
+      <select id="diplome_cuisinier">
+        <?php
+          while($ligne = $request->fetch()){ ?>
+            <option>
+            <?= $ligne['diplome']; ?>
+            </option>
+        <?php } ?>
+      </select>
+    </div>
+    <div>
+      <input type="submit" name="valider">
+    </div>
+  </fieldset>
+</form>
+
+<?php include('pieddepage.php'); ?>
+
+
+
+
+
+
+
+
+<?php
+// Value form
+function valueForm($field) {
+  if (!empty($_POST[$field])) {
+    return $_POST[$field];
+  };
+}
+
+?>
