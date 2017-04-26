ccb626682e127bf52b7ff0cd5c5a2396620759a2
Kris <chrisparis.eh@gmail.com>
2017-04-26 18:39:20 +0200

    affichage du cuisinier à modifier

--- update_cuisinier.php
+++ update_cuisinier.php
@@ -1,28 +1,42 @@
-<?php include('entete.php'); ?>
+<?php
+include('entete.php');
+
+// on récupère et on stocke dans une variable $id_cuisinier l' id du cuisinier à modifier
+$id = $_REQUEST['id']; ?>
 
 
-<form action="update_cuisinier_post.php" method="post">
+<form method="post">
   <fieldset id="modifier_cuisinier">
-  <legend>Modifier les informations d'un cuisinier</legend>
+  <legend>Modifier les informations d'un cuisinier :
+    <?php
+//afficher le cuisinier à modifier
+      $sql = 'SELECT * FROM cuisinier WHERE  id = :id';
+      $request = $base->prepare($sql);
+      $request->bindValue(":id", $id);
+      $request->execute();
+      while ($ligne = $request->fetch()) {
+        echo '<strong>' . $ligne['nom'] . '<strong>';
+    } ?>
+  </legend>
 
     <div>
-      <label for="nom_cuisinier">Nom du cuisinier :</label>
-      <input type="text" name="nom_cuisinier" id="nom_cuisinier">
+      <label for="nom_cuisinier">Nom :</label>
+      <input type="text" name="nom_cuisinier" id="nom_cuisinier" />
     </div>
 
     <div>
       <label for="salaire">salaire :</label>
       <input type="number" name="salaire" id="salaire" value="" />
     </div>
-<?php
-$sql = 'SELECT restaurant.nom AS restaurant FROM restaurant';
-$request = $base->prepare($sql);
-$request->execute();
-?>
+
     <div>
       <label for="restaurant_cuisinier">Restaurant du cuisinier :</label>
       <select id="restaurant_cuisinier">
         <?php
+//afficher la liste des restaurants
+        $sql = 'SELECT restaurant.nom AS restaurant FROM restaurant';
+        $request = $base->prepare($sql);
+        $request->execute();
           while($ligne = $request->fetch()){ ?>
             <option>
             <?= $ligne['restaurant']; ?>
@@ -30,15 +44,16 @@ $request->execute();
         <?php } ?>
       </select>
     </div>
-<?php
-$sql = 'SELECT DISTINCT diplome.nom AS diplome FROM diplome';
-$request = $base->prepare($sql);
-$request->execute();
-?>
+
     <div>
       <label for="diplome_cuisinier">Diplôme du cuisinier :</label>
       <select id="diplome_cuisinier">
         <?php
+//afficher la liste des diplômes
+        $sql = 'SELECT DISTINCT diplome.nom AS diplome FROM diplome';
+        $request = $base->prepare($sql);
+        //$request->bindValue(":id-cuisinier", $id_cuisinier);
+        $request->execute();
           while($ligne = $request->fetch()){ ?>
             <option>
             <?= $ligne['diplome']; ?>
@@ -52,4 +67,36 @@ $request->execute();
   </fieldset>
 </form>
 
-<?php include('pieddepage.php'); ?>
+<?php
+// on recupère les id des restaurants et des diplomes
+$sql = 'SELECT
+          cuisinier.id AS cuisinier_id,
+          cuisinier.nom AS cuisinier,
+          restaurant.id AS restaurant_id,
+          restaurant.nom AS restaurant,
+          diplome.id AS diplome_id,
+          diplome.nom AS diplome
+        FROM cuisinier
+          JOIN diplome ON diplome.id = id_diplome
+          JOIN restaurant ON restaurant.id = id_restaurant';
+
+$requete = $base ->prepare($sql);
+$requete->execute();
+while($ligne = $requete->fetch()) {
+  $id_restaurant = $ligne['restaurant_id'];
+  $id_diplome = $ligne['diplome_id'];
+};
+
+/*
+if(!empty($_POST)) {
+  $sql = 'UPDATE cuisinier(nom, salaire, id_restaurant, id_diplome) VALUES(:nom, :salaire, :id_restaurant, :id_diplome) WHERE id=:id-cuisinier';
+  $request->prepare($sql);
+  $request->execute(array(
+    'nom'           => $_POST['nom'],
+    'salaire'       => $_POST['salaire'],
+    'id_restaurant' => $id_restaurant,
+    'id_diplome'    => $id_diplome
+  ));
+}
+*/
+include('pieddepage.php'); ?>
