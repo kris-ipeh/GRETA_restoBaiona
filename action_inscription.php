41b76d54596ea6a285d4164a7bf25bbac0615807
Google <itadakimeus@gmail.com>
2017-04-27 11:23:45 +0200

    inscription/connection/creation de table

--- action_inscription.php
+++ action_inscription.php
@@ -2,18 +2,21 @@
 	include('entete.php');
 
 
-
+	$noom='SELECT * FROM utilisateur WHERE nom';
 
 	$nom=$_POST['nom'];
 	$mdp=$_POST['mdp'];
 			
-				$sql='INSERT INTO utilisateur(nom,mdp) VALUES (:nom,:mdp)';
+				$sql='INSERT INTO utilisateur (nom,mdp) VALUES (:nom,:mdp)';
 				$request = $base->prepare($sql);
 				$request -> bindvalue(':nom',$nom);
 				$request -> bindvalue(':mdp',$mdp);
 				$request -> execute();
 
+if ($nom=$noom) {
+	echo 'nom deja utiliser'
+}
 ?>
 
 
-<p>Bonjour <?=$nom?>, vous êtes bien inscrit.</p>
+<p>Bonjour <?=$nom?>, vous êtes bien inscrit.<a href="connection.php">Continuer</a></p>
--- resto.sql
+++ resto.sql
@@ -70,3 +70,28 @@ values
 ('Bernard Loiseau', 1750, 4, 3),
 ('Guy Savoy', 1250, 5, 1),
 ('Alain Ducasse', 2250, 1, 1);
+
+
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Client :  127.0.0.1
+-- Généré le :  Mar 25 Avril 2017 à 12:37
+-- Version du serveur :  5.7.14
+-- Version de PHP :  5.6.25
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+
+
+create table utilisateur(
+ id int not null auto_increment,
+ nom varchar(100) not null,
+ mdp varchar(100) not null,
+ primary key(id)
+) engine=InnoDB;
+
+
