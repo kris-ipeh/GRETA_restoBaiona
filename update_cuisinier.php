c41294fd4719056e46bc3a41f90c3b071993560d
Sébastien Ducoulombier <seb@ldd.fr>
2017-04-27 11:03:12 +0200

    debuggé la modification d'un cuisinier

--- update_cuisinier.php
+++ update_cuisinier.php
@@ -2,62 +2,81 @@
 include('entete.php');
 
 // on récupère et on stocke dans une variable l'id du cuisinier à modifier
+// l'id a été passé dans l'URL : "update_cuisinier.php?id=42" par exemple
 $id = $_REQUEST['id']; ?>
 
 <!-- formulaire -->
-<form method="post">
+<form action="action_update_cuisinier.php">
+  <!-- on transmet au fichier PHP de destination l'id du cuisinier -->
+  <input type="hidden" name="id" value="<?php echo $id ?>">
   <fieldset id="modifier_cuisinier">
   <legend>Modifier les informations d'un cuisinier :
-    <?php
-//afficher le cuisinier à modifier
-      $sql = 'SELECT * FROM cuisinier WHERE  id =:id';
+  </legend>
+  <?php
+      //afficher le cuisinier à modifier
+      $sql = 'SELECT * FROM cuisinier WHERE id =:id';
       $request = $base->prepare($sql);
       $request->bindValue(":id", $id);
       $request->execute();
-      while ($ligne = $request->fetch()) {
-        echo '<strong>' . $ligne['nom'] . '<strong>';
-    ?>
-  </legend>
-    <div>
-      <label for="nom">Nom :</label>
-      <input type="text" name="nom" id="nom" value="<?php echo($ligne['nom']); ?>" />
+      $ligneCuisinier = $request->fetch();
+      ?>
+      <div>
+        <label>Nom :
+          <input name="nom"
+                 value="<?php echo($ligneCuisinier['nom']); ?>" />
+        </label>
+      </div>
+    
+      <div>
+        <label>salaire :
+           <input type="number" name="salaire"
+                  value="<?php echo($ligneCuisinier['salaire']); ?>"/>
+        </label>
+      </div>
+      <div>
+      <label>Restaurant :
+       <select name="id_restaurant">
+        <?php
+        //afficher la liste des restaurants
+        $sql = 'SELECT id, nom FROM restaurant';
+        $request = $base->prepare($sql);
+        $request->execute();
+        while($ligneRestaurant = $request->fetch()){ ?>
+            <option value="<?php echo $ligneRestaurant['id']; ?>"
+            <?php
+            if ($ligneRestaurant['id'] == $ligneCuisinier['id_restaurant']) {
+               echo 'selected';
+            }
+            ?>
+            >
+             <?= $ligneRestaurant['nom']; ?>
+           </option>
+        <?php } ?>
+       </select>
+      </label>
     </div>
 
     <div>
-      <label for="salaire">salaire :</label>
-      <input type="number" name="salaire" id="salaire"  value="<?php echo($ligne['salaire']); ?>"/>
-    </div>
-    <?php } ?>
-    <div>
-      <label for="restaurant_cuisinier">Restaurant :</label>
-      <select id="restaurant_cuisinier" />
+     <label>Diplôme :
+      <select name="id_diplome" />
         <?php
-//afficher la liste des restaurants
-        $sql = 'SELECT restaurant.nom AS restaurant FROM restaurant';
+        //afficher la liste des diplômes
+        $sql = 'SELECT id, nom FROM diplome';
         $request = $base->prepare($sql);
         $request->execute();
-          while($ligne = $request->fetch()){ ?>
-            <option>
-            <?= $ligne['restaurant']; ?>
-            </option>
+        while($ligneDiplome = $request->fetch()){ ?>
+          <option value="<?php echo $ligneDiplome['id']; ?>"
+          <?php
+            if ($ligneDiplome['id'] == $ligneCuisinier['id_diplome']) {
+               echo 'selected';
+            }
+          ?>
+          >
+            <?php echo $ligneDiplome['nom']; ?>
+          </option>
         <?php } ?>
       </select>
-    </div>
-
-    <div>
-      <label for="diplome_cuisinier">Diplôme :</label>
-      <select id="diplome_cuisinier" />
-        <?php
-//afficher la liste des diplômes
-        $sql = 'SELECT DISTINCT diplome.nom AS diplome FROM diplome';
-        $request = $base->prepare($sql);
-        $request->execute();
-          while($ligne = $request->fetch()){ ?>
-            <option>
-            <?= $ligne['diplome']; ?>
-            </option>
-        <?php } ?>
-      </select>
+     </label>
     </div>
     <div>
       <input type="submit" name="valider">
@@ -65,41 +84,4 @@ $id = $_REQUEST['id']; ?>
   </fieldset>
 </form>
 
-<?php
-// on recupère les id des restaurants et des diplomes
-$sql = 'SELECT
-          cuisinier.id AS cuisinier_id,
-          cuisinier.nom AS cuisinier,
-          restaurant.id AS restaurant_id,
-          restaurant.nom AS restaurant,
-          diplome.id AS diplome_id,
-          diplome.nom AS diplome
-        FROM cuisinier
-          JOIN diplome ON diplome.id = id_diplome
-          JOIN restaurant ON restaurant.id = id_restaurant';
-$request = $base ->prepare($sql);
-$request->execute();
-while($ligne = $request->fetch()) {
-  $id_restaurant = $ligne['restaurant_id'];
-  $id_diplome = $ligne['diplome_id'];
-};
-
-//on effectue les modifications dans la bdd
-if(!empty($_POST)) {
-  $request = $base->prepare(
-        'UPDATE cuisinier
-          SET nom = :nom,
-              salaire = :salaire,
-              id_restaurant = :id_restaurant,
-              id_diplome = :id_diplome
-          WHERE id = :id');
-
-  $request->bindValue(':nom', $_REQUEST['nom']);
-  $request->bindValue(':salaire', $_REQUEST['salaire']);
-  $request->bindValue(':id_restaurant', $id_restaurant);
-  $request->bindValue(':id_diplome', $id_diplome);
-  $request->bindValue(':id', $id);
-  $request->execute();
-};
-
-include('pieddepage.php'); ?>
+<?php include('pieddepage.php'); ?>
