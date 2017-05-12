7b2be74c02d7849826b9332a3d8957f8c6e4a4e9
kris <chrisparis.eh@gmail.com>
2017-05-12 12:28:08 +0200

    création de la fiche cuisinier

---
+++ fiche_cuisinier.php
@@ -0,0 +1,31 @@
+<?php include("entete.php");
+
+$id = $_REQUEST['id'];
+//var_dump($id);
+
+$requete = $base->prepare("
+    SELECT
+      cuisinier.nom AS nom_cuisinier,
+      cuisinier.salaire AS salaire,
+      diplome.nom AS diplome,
+      cuisinier.id AS id_cuisinier,
+      restaurant.nom AS restaurant
+    FROM cuisinier
+      JOIN diplome ON diplome.id=id_diplome
+      JOIN restaurant ON restaurant.id=id_restaurant
+    WHERE cuisinier.id=:id
+ ");
+
+$requete->bindParam(':id', $id);
+$requete->execute();
+$ligne = $requete->fetch();
+
+echo '<h2>'.$ligne['nom_cuisinier'].'</h2>';
+echo '<p> est titulaire du '.$ligne['diplome'].',</p>';
+echo '<p> travaille dans le restaurant : '.$ligne['restaurant'].',</p>';
+echo '<p> pour un salaire de '.$ligne['salaire'].'€.</p>';
+
+echo '<a href="update_cuisinier.php?id='.$ligne['id_cuisinier'].'">Modifier</a>';
+
+
+?>
