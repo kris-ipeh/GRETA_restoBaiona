770bb21314f7badffb88a8cd6045d04da23e8777
root <root@greta.ldd.fr>
2017-06-02 15:33:41 +0200

    Restreint les permissions sur la table diplome

--- permissions.sql
+++ permissions.sql
@@ -4,5 +4,14 @@ CREATE user 'siteweb'@'localhost' IDENTIFIED BY '1234';
 
   -- Pour permettre les permissions
   GRANT SELECT, UPDATE, INSERT, DELETE
-    ON restov2.*
+    ON restov2.quartier
+    TO 'siteweb'@'localhost';
+  GRANT SELECT, UPDATE, INSERT, DELETE
+    ON restov2.restaurant
+    TO 'siteweb'@'localhost';
+  GRANT SELECT, UPDATE, INSERT, DELETE
+    ON restov2.cuisinier
+    TO 'siteweb'@'localhost';
+  GRANT SELECT, INSERT
+    ON restov2.diplome
     TO 'siteweb'@'localhost';
